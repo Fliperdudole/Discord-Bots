01e1ec79e6d3a3bd4f3c9f8dac967b5f928a8389
Brandon Sandoval <brandon.sandoval123@gmail.com>
2023-06-04 00:05:20 -0500

    created patch reader/ saver

---
+++ Riot Patch Notes/Main_v2/ValorantPN.py
@@ -0,0 +1,13 @@
+#Programmer: Brandon Sandoval
+#Date Started: 6/3/23
+#Purpose: This helper file will contain all the necessary variables in order 
+#         for the main file to work for the specified game
+
+
+import saveVar
+
+# URL for the Valorant Patch Notes website
+Valorant_URL = 'https://playvalorant.com/en-us/news/game-updates/valorant-patch-notes-{}-{}/'
+
+# File to store the last patch number
+LAST_PATCH_FILE = 'last_valorant_patch.txt'
