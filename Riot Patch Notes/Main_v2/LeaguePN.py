72afdf9fcb8590f68d384cb2b64bf1c710c1ff38
Brandon Sandoval <brandon.sandoval123@gmail.com>
2023-06-05 13:32:44 -0500

    Added Role-Creating Function Added ALOT of comment

--- Riot Patch Notes/Main_v2/LeaguePN.py
+++ Riot Patch Notes/Main_v2/LeaguePN.py
@@ -3,6 +3,8 @@
 #Purpose: This helper file will contain all the necessary variables/functions in order 
 #         for the main file to work for the specified game
 
+
+# Import libraries
 import saveVar
 import discord
 import requests
@@ -34,17 +36,18 @@ league_response = requests.get(league_url)
 
 
 
-
+# Used as a bool flag
 checking_patch = True
 
 
 
-
+# This function, using the requests module, checks the website and sends in the notfication channel
 async def check_League_Patch(CHANNEL_ID, MAX_PATCH, client):
     global current_patch, patch_tries, checking_patch
 
     url = URL.format(current_season,current_patch)
     
+    # Request the website through HTML
     response = requests.get(url)
 
     # Check if the page exists (returns 200 status code)
@@ -54,21 +57,26 @@ async def check_League_Patch(CHANNEL_ID, MAX_PATCH, client):
 
         channel = client.get_channel(CHANNEL_ID)
         
+        # if the role exists then send a message mentioning the role
         try:
             role = discord.utils.get(channel.guild.roles, name=role_name)
             if role:
                 message = f"{role.mention}\n{leagueMessage} "
                 
             await channel.send(message)
-        except discord.Forbidden:
+            
+        except discord.Forbidden:# Bot doesnt have the right permissions in the server to send messages
             print("The bot doesn't have permission to send messages in the channel.")
             return
         
         current_patch += 1
+
+        # Save the last patch in a text file
         saveVar.write_last_patch(LAST_PATCH_FILE,current_patch)
 
     else:
 
+        # Prints in the terminal that no update was found
         print(f"No update found for patch {current_season}.{current_patch}.")
         
         current_patch +=1
@@ -76,7 +84,7 @@ async def check_League_Patch(CHANNEL_ID, MAX_PATCH, client):
         
         patch_tries +=1
 
-        # Check if the current patch exceeds a limit
+        # Check if the current patch exceeds a limit as Riot likes to jump patches at times
         if patch_tries > MAX_PATCH:
             print("Reached the maximum patch limit.")
             
